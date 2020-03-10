ed84b1b6a5287b91dbec2b95ddc8c42f97931084
Aman Bhargava <amanb2000@gmail.com>
2020-03-10 19:06:56 -0400

    Update axis_swim.py, ready for collaboration

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Code for a simulated Sequential Wave Imprinter (SWIM) in Python3/Matplotlib.
 
 See `requirements.txt` for list of dependencies.
+
+Files in code: 
+1. `axis_swim.py`: SWIMulator with axis on them (per Steve's advice). 
+2. `radial_swim.py`: SWIMulator with no axis. Depreciated version.
--- code/axis_swim.py
+++ code/axis_swim.py
@@ -1,18 +1,3 @@
-
-"""
-Notebook for streaming data from a microphone in realtime
-
-audio is captured using pyaudio
-then converted from binary data to ints using struct
-then displayed using matplotlib
-
-if you don't have pyaudio, then run
-
->>> pip install pyaudio
-
-note: with 2048 samples per chunk, I'm getting 20FPS
-"""
-
 import pyaudio
 import os
 import struct
@@ -22,7 +7,6 @@ import time
 from tkinter import TclError
 import math
 
-# use this backend to display in separate Tk window
 # %matplotlib tk
 
 # constants
@@ -58,22 +42,28 @@ print(x.shape)
 
 print(x)
 # create a line object with random data
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2, color='#330033')
+line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=1, color='#330033')
+
 
 # basic formatting for the axes
 ax.set_title('')
 # ax.set_xlabel('samples')
 # ax.set_ylabel('volume')
 ax.set_ylim(-1, 259)
-ax.set_xticks([]) 
-ax.set_yticks([]) 
+ax.set_rticks([])
+# ax.set_yticks([])
+ax.set_xticks(np.arange(0,2.0*np.pi, np.pi/2.0))
+ax.set_yticklabels([])
+ax.set_xticklabels([])
+
+
+
+
+ax.grid(True, color='#000000', linestyle='-', linewidth=1)
+
 
 ax.set_facecolor((1,1,1))
-# ax.set_axis_bgcolor((0,0,0))
-# ax.set_xlim(0, 360)
-# plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
 
-# show the plot
 plt.show(block=False)
 
 print('stream started')
@@ -85,11 +75,11 @@ start_time = time.time()
 # smallest = 999;
 
 while True:
-    # binary data
-    data = stream.read(CHUNK, exception_on_overflow=False)  
+    data = stream.read(CHUNK, exception_on_overflow=False) 
     
     # convert data to integers, make np array, then offset it by 127
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+    # Note that this only works on low-gain signals. If you are interested in making this work on high-gain signals, you can play around with this line.
+    data_int = struct.unpack(str(2 * CHUNK) + 'B', data) 
     
     # create np array and offset by 128
     data_np = np.array(data_int, dtype='b')[::2] + 129
